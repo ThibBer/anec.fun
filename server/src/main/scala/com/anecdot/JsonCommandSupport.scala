7e406ec23f6db6776e245bb9edbc3e1bb1001a53
Victor Santelé <victor.santele@gmail.com>
2024-12-05 23:55:42 +0100

    add missing command

--- server/src/main/scala/com/anecdot/JsonCommandSupport.scala
+++ server/src/main/scala/com/anecdot/JsonCommandSupport.scala
@@ -34,6 +34,7 @@ trait JsonCommandSupport extends SprayJsonSupport with DefaultJsonProtocol {
       case cmd: SetGameModeCommand                         => cmd.toJson
       case com.anecdot.VoteSubmittedNotification(_, _)    => ???
       case com.anecdot.GameStateChangedNotification(_, _) => ???
+      case com.anecdot.StopRoundCommand(_) => ???
     }
 
     def read(json: JsValue): Command = {
